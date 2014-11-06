c8fba3ade2dbb450a06c17e8b477a3a4c11be666
Robert Anderson <robert.anderson@xodigital.com.au>
2014-11-06 16:30:50 +1100

    1.0.2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Formerly 1.0.1
+# Formerly 1.0.2
 
 ## Installing
 
--- formerly/FormerlyPlugin.php
+++ formerly/FormerlyPlugin.php
@@ -10,7 +10,7 @@ class FormerlyPlugin extends BasePlugin
 
 	public function getVersion()
 	{
-	    return '1.0.1';
+	    return '1.0.2';
 	}
 
 	public function getDeveloper()
